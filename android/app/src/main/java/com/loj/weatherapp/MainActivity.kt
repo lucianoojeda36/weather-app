3d61883cc19e7bef37c0970551eceaed57334e4d
Luciano Ojeda <lucianoojeda36@gmail.com>
2026-03-28 20:21:06 -0300

    update package name

--- android/app/src/main/java/com/loj/weatherapp/MainActivity.kt
+++ android/app/src/main/java/com/loj/weatherapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.weatherapp
+package com.loj.weatherapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/loj/weatherapp/MainApplication.kt
+++ android/app/src/main/java/com/loj/weatherapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.weatherapp
+package com.loj.weatherapp
 
 import android.app.Application
 import com.facebook.react.PackageList
